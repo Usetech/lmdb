36c49f22d6ac0fae94951e19787be16926855d86
Pavel Parkhomenko <pparkhomenko@usetech.ru>
2013-08-23 18:03:34 +0400

    Групповая установка проверенности

--- core/admin/tools.py
+++ core/admin/tools.py
@@ -16,3 +16,12 @@ class StatusAdminMixin(object):
     get_status_name.admin_order_field = 'status'
     get_status_name.short_description = u'Состояние'
     get_status_name.allow_tags = True
+
+    def mark_as_checked(self, request, queryset):
+        for object in queryset:
+            object.status = 'OK'
+            object.save()
+
+    mark_as_checked.short_description = u'Отметить как проверенные'
+
+    actions = ['mark_as_checked']
